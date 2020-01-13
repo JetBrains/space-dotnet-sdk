1f53b640542f3d06652e53743d6e6474167fade4
Maarten Balliauw <maarten@maartenballiauw.be>
2020-01-13 09:48:37 +0100

    Add logout

---
+++ src/SpaceDotNet.Samples.Web/Pages/Logout.cshtml.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SpaceDotNet.Samples.Web.Pages
+{
+    [Authorize]
+    public class Logout : PageModel
+    {
+        public async Task<IActionResult> OnGet()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("Index");
+        }
+    }
+}
