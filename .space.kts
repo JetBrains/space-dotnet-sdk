fcf9149e65bdcd35e9753992866b67f14c0f3b5e
Maarten Balliauw <maarten.balliauw@jetbrains.com>
2020-03-18 11:15:57 +0000

    Customize entry point

--- .space.kts
+++ .space.kts
@@ -1,5 +1,9 @@
 job("Run NUKE build") {
     container("mcr.microsoft.com/dotnet/core/sdk:3.1") {
-        args("build.sh")
+        mountDir = "/mnt/mySpace"
+        workDir = "/mnt/mySpace/work"
+        user = "root"
+        entrypoint("/bin/sh")
+        args("./build.sh")
     }
 }
