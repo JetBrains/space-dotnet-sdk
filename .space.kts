176ad4c72b676257749c5a2b1cd5688b56c13200
Maarten Balliauw <maarten.balliauw@jetbrains.com>
2020-03-18 12:39:03 +0000

    Add CPU/memory

--- .space.kts
+++ .space.kts
@@ -1,5 +1,10 @@
 job("Run NUKE build") {
     container("mcr.microsoft.com/dotnet/core/sdk:3.1-bionic") {
+        resources {
+            cpu = 2048
+            memory = 2048
+        }
+
         mountDir = "/mnt/mySpace"
         workDir = "/mnt/mySpace/work"
         user = "root"
