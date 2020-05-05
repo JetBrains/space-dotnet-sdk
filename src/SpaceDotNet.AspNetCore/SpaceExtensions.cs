1a77bf409de76a276b6e1d404c89233bf79b63ed
Maarten Balliauw <maarten@maartenballiauw.be>
2020-05-05 17:23:32 +0200

    Add ASP.NET Core utilities to a separate package

--- src/SpaceDotNet.AspNetCore/SpaceExtensions.cs
+++ src/SpaceDotNet.AspNetCore/SpaceExtensions.cs
@@ -3,10 +3,10 @@ using System.Linq;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using SpaceDotNet.Client;
 
-// ReSharper disable CheckNamespace
+// ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection
 {
-    public static class ServiceCollectionExtensions
+    public static class SpaceExtensions
     {
         public static IServiceCollection AddSpaceClientApi(this IServiceCollection services)
         {
@@ -14,7 +14,11 @@ namespace Microsoft.Extensions.DependencyInjection
             {
                 throw new ArgumentNullException(nameof(services));
             }
-
+            
+            // Add prerequisites
+            services.AddHttpClient();
+            
+            // Add Space ...Client types
             var spaceClientAssembly = typeof(TeamDirectoryClient).Assembly;
             foreach (var type in spaceClientAssembly.GetTypes().Where(t => !t.IsAbstract && t.Name.EndsWith("Client")))
             {
