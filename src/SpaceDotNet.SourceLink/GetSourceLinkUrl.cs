0832773fa9e3e75fd4886e7ae652a5d1122bd3e1
Maarten Balliauw <maarten@maartenballiauw.be>
2020-05-06 15:39:13 +0200

    Sourcelink spike

---
+++ src/SpaceDotNet.SourceLink/GetSourceLinkUrl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using JetBrains.Annotations;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Tasks.SourceControl;
+
+namespace SpaceDotNet.SourceLink
+{
+    /// <summary>
+    /// The task calculates SourceLink URL for a given SourceRoot.
+    /// If the SourceRoot is associated with a git repository with a recognized domain the <see cref="SourceLinkUrl"/>
+    /// output property is set to the content URL corresponding to the domain, otherwise it is set to string "N/A".
+    /// </summary>
+    [PublicAPI]
+    public sealed class GetSourceLinkUrl : GetSourceLinkUrlGitTask
+    {
+        protected override string HostsItemGroupName => "SourceLinkSpaceHost";
+        protected override string ProviderDisplayName => "Space";
+
+        protected override string? BuildSourceLinkUrl(Uri contentUri, Uri gitUri, string relativeUrl, string revisionId, ITaskItem? hostItem)
+            => UriUtilities.Combine(UriUtilities.Combine(contentUri.ToString(), relativeUrl), "raw/" + revisionId + "/*");
+    }
+}
---
+++ src/SpaceDotNet.SourceLink/TranslateRepositoryUrls.cs
@@ -0,0 +1,11 @@
+using JetBrains.Annotations;
+using Microsoft.Build.Tasks.SourceControl;
+
+namespace SpaceDotNet.SourceLink
+{
+    [PublicAPI]
+    public sealed class TranslateRepositoryUrls 
+        : TranslateRepositoryUrlsGitTask
+    {
+    }
+}
