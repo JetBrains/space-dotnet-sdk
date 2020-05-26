81a3df4aa2d65a3e857150f6482d6efc45702cef
Maarten Balliauw <maarten@maartenballiauw.be>
2020-05-26 13:47:06 +0200

    Update sample

--- src/SpaceDotNet.Samples.Web/Pages/Index.cshtml.cs
+++ src/SpaceDotNet.Samples.Web/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -6,6 +7,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using SpaceDotNet.Client;
+using SpaceDotNet.Client.CPrincipalExtensions;
+using SpaceDotNet.Client.CUserPrincipalDetailsExtensions;
+using SpaceDotNet.Client.DTOMeetingExtensions;
+using SpaceDotNet.Client.IssueExtensions;
+using SpaceDotNet.Client.M2ChannelRecordExtensions;
+using SpaceDotNet.Client.MessageInfoExtensions;
 using SpaceDotNet.Client.TDMemberProfileExtensions;
 using SpaceDotNet.Client.TDProfileNameExtensions;
 using SpaceDotNet.Client.TodoItemRecordExtensions;
@@ -22,37 +29,47 @@ namespace SpaceDotNet.Samples.Web.Pages
         private readonly ProjectClient _projectClient;
         private readonly ToDoItemClient _todoClient;
         private readonly TeamDirectoryClient _teamDirectoryClient;
+        private readonly CalendarClient _calendarClient;
 
         public TDMemberProfileDto? MemberProfile { get; set; }
+        public List<DTOMeetingDto> MeetingsToday { get; set; } = new List<DTOMeetingDto>();
         public int IssuesCreatedThisWeek { get; set; }
         public int IssuesResolvedThisWeek { get; set; }
         public int ReviewsCreatedThisWeek { get; set; }
         public int ReviewsParticipatedThisWeek { get; set; }
         public int TodoClosedThisWeek { get; set; }
+        public int MeetingsThisWeek { get; set; }
         
         public IndexModel(
             IConfiguration configuration, 
             ILogger<IndexModel> logger,
             ProjectClient projectClient,
             ToDoItemClient todoClient,
-            TeamDirectoryClient teamDirectoryClient)
+            TeamDirectoryClient teamDirectoryClient,
+            CalendarClient calendarClient)
         {
             _configuration = configuration;
             _logger = logger;
             _projectClient = projectClient;
             _todoClient = todoClient;
             _teamDirectoryClient = teamDirectoryClient;
+            _calendarClient = calendarClient;
         }
 
         public async Task OnGet()
         {
+            #region Example with full field definitions
+            
             MemberProfile = await _teamDirectoryClient.Profiles.Me.GetMe(partial => partial
-                .AddAllFieldNames() // adds all fields for the current partial request
+                .AddAllFieldNames() // adds all fields for the current partial request (one level)
                 .WithId()
                 .WithName(name => name
                     .WithFirstName()
                     .WithLastName())
-                .WithEmails());
+                .WithEmails()
+                .WithMessengers());
+            
+            #endregion
             
             MemberProfile = await _teamDirectoryClient.Profiles.Me.GetMe();
             
@@ -64,7 +81,8 @@ namespace SpaceDotNet.Samples.Web.Pages
             var reviewsCreatedThisWeek = 0;
             var reviewsParticipatedThisWeek = 0;
             var todoClosedThisWeek = 0;
-
+            var meetingsThisWeek = 0;
+            
             await foreach (var projectDto in BatchEnumerator.AllItems(skip => _projectClient.GetAllProjectsByMember(MemberProfile.Id, skip: skip)))
             {
                 try
@@ -72,7 +90,24 @@ namespace SpaceDotNet.Samples.Web.Pages
                     // Check # of issues resolved this week
                     var issueStatuses = await _projectClient.Planning.Issues.Statuses.GetAllIssueStatuses(projectDto.Id);
                 
-                    await foreach (var issueDto in BatchEnumerator.AllItems(skip => _projectClient.Planning.Issues.GetAllIssues(projectDto.Id, issueStatuses.Select(it => it.Id).ToList(), IssuesSorting.UPDATED, descending: true, skip: skip)))
+                    await foreach (var issueDto in BatchEnumerator.AllItems(skip => _projectClient.Planning.Issues.GetAllIssues(projectDto.Id, issueStatuses.Select(it => it.Id).ToList(), IssuesSorting.UPDATED, descending: true, skip: skip, partialBuilder: partial => partial
+                        .WithNext()
+                        .WithTotalCount()
+                        .WithData(issue => issue
+                            .AddAllFieldNames()
+                            .WithCreationTime()
+                            .WithCreatedBy(createdBy => createdBy
+                                .WithDetails(details => details
+                                    .WithInherited<CUserPrincipalDetailsDto>(detailsDto => detailsDto
+                                        .WithUser(user => user
+                                            .WithId()))))
+                            .WithStatus()
+                            .WithAssignee(assignee => assignee
+                                .WithId())
+                            .WithChannel(channel => channel
+                                .WithLastMessage(lastMessage => lastMessage
+                                    .WithText()
+                                    .WithTime()))))))
                     {
                         var created = issueDto.CreationTime.AsDateTime();
                         var lastUpdated = issueDto.Channel.LastMessage?.Time.AsDateTime() ?? created;
@@ -165,8 +200,8 @@ namespace SpaceDotNet.Samples.Web.Pages
                 await foreach (var todoDto in BatchEnumerator.AllItems(skip => _todoClient.GetAllToDoItems(from: weekStart.AsSpaceDate(), skip: skip, partialBuilder: partial => partial
                     .WithNext()
                     .WithTotalCount()
-                    .WithData(x => 
-                        x.WithStatus()))))
+                    .WithData(todoItem => todoItem
+                        .WithStatus()))))
                 {
                     if (todoDto.Status == "Closed")
                     {
@@ -179,6 +214,31 @@ namespace SpaceDotNet.Samples.Web.Pages
                 _logger.LogError("An error occurred", ex);
             }
 
+            try
+            {
+                await foreach (var meetingDto in BatchEnumerator.AllItems(skip => _calendarClient.Meetings.GetAllMeetings("", new List<string>(), new List<string> { MemberProfile.Id }, new List<string>(), true, false, startingAfter: weekStart.AsSpaceTime(), endingBefore: weekEnd.AsSpaceTime(), skip: skip, partialBuilder: partial => partial
+                    .WithNext()
+                    .WithTotalCount()
+                    .WithData(meeting => meeting
+                        .AddAllFieldNames()
+                        .WithId()
+                        .WithSummary()
+                        .WithDescription()
+                        .WithConferenceLink()))))
+                {
+                    meetingsThisWeek++;
+
+                    if (meetingDto.OccurrenceRule.Start.AsDateTime().Date == DateTime.UtcNow.Date)
+                    {
+                        MeetingsToday.Add(meetingDto);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("An error occurred", ex);
+            }
+            
 
             // Issues
             // Reviews created
@@ -194,6 +254,7 @@ namespace SpaceDotNet.Samples.Web.Pages
             ReviewsCreatedThisWeek = reviewsCreatedThisWeek;
             ReviewsParticipatedThisWeek = reviewsParticipatedThisWeek;
             TodoClosedThisWeek = todoClosedThisWeek;
+            MeetingsThisWeek = meetingsThisWeek;
         }
 
         public static DateTime StartOfWeek(DateTime dt, DayOfWeek startOfWeek)
