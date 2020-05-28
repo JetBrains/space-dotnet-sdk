8df33ec9f1c9f9ff814fa4b6d22d9fc334cb2b69
Maarten Balliauw <maarten@maartenballiauw.be>
2020-05-28 13:57:01 +0200

    Add command-line sample

---
+++ SpaceDotNet.Samples.CommandLine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SpaceDotNet.Client;
+using SpaceDotNet.Client.TDMemberProfileExtensions;
+using SpaceDotNet.Common;
+
+namespace SpaceDotNet.Samples.CommandLine
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Create a connection using a service account.
+            // NOTE: Service accounts do not have access to all operations in Space!
+            var connection = new ClientCredentialsConnection(
+                Environment.GetEnvironmentVariable("JB_SPACE_API_URL")!,
+                Environment.GetEnvironmentVariable("JB_SPACE_CLIENT_ID")!,
+                Environment.GetEnvironmentVariable("JB_SPACE_CLIENT_SECRET")!,
+                new HttpClient());
+            
+            // Get all profiles with their names
+            var teamDirectoryClient = new TeamDirectoryClient(connection);
+            await foreach (var profile in BatchEnumerator.AllItems(skip => teamDirectoryClient.Profiles.GetAllProfiles("", false, false, skip: skip, partialBuilder: partial => partial
+                .WithNext()
+                .WithTotalCount()
+                .WithData(data => data
+                    .WithId()
+                    .WithName()))))
+            {
+                Console.WriteLine($"{profile.Name.FirstName} {profile.Name.LastName}");
+            }
+        }
+    }
+}
