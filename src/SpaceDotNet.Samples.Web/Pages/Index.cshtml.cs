a88dd11b4867331921fa974a7ee7e6d2ec2ae48e
Maarten Balliauw <maarten@maartenballiauw.be>
2020-06-12 07:59:30 +0200

    Update sample

--- src/SpaceDotNet.Samples.Web/Pages/Index.cshtml.cs
+++ src/SpaceDotNet.Samples.Web/Pages/Index.cshtml.cs
@@ -245,7 +245,7 @@ namespace SpaceDotNet.Samples.Web.Pages
 
             try
             {
-                await foreach (var meetingDto in _calendarClient.Meetings.GetAllMeetingsAsyncEnumerable("", new List<string>(), new List<string> { MemberProfile.Id }, new List<string>(), true, false, startingAfter: weekStart.AsSpaceTime(), endingBefore: weekEnd.AsSpaceTime(), partial: _ => _
+                await foreach (var meetingDto in _calendarClient.Meetings.GetAllMeetingsAsyncEnumerable("", new List<string>(), new List<string> { MemberProfile.Id }, new List<string>(), true, false, true, startingAfter: weekStart.AsSpaceTime(), endingBefore: weekEnd.AsSpaceTime(), partial: _ => _
                     .WithAllFieldsWildcard()
                     .WithId()
                     .WithSummary()
