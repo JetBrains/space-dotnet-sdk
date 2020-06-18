df91319ba4ea47013571041243a94a7c8d97ffd3
Maarten Balliauw <maarten@maartenballiauw.be>
2020-06-18 13:14:22 +0200

    Equality members and comparison for SpaceTime / SpaceDate

--- src/SpaceDotNet.Common/Types/SpaceDate.cs
+++ src/SpaceDotNet.Common/Types/SpaceDate.cs
@@ -5,7 +5,7 @@ using JetBrains.Annotations;
 namespace SpaceDotNet.Common.Types
 {
     [PublicAPI]
-    public class SpaceDate
+    public class SpaceDate : IComparable<SpaceDate>, IComparable
     {
         [JsonPropertyName("iso")]
         public string Iso { get; set; } = default!;
@@ -24,6 +24,48 @@ namespace SpaceDotNet.Common.Types
         public override string ToString()
         {
             return AsDateTime().ToString("yyyy-MM-dd");
+        }
+
+        protected bool Equals(SpaceDate other)
+        {
+            return string.Equals(Iso, other.Iso, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((SpaceDate) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return StringComparer.OrdinalIgnoreCase.GetHashCode(Iso);
+        }
+
+        public static bool operator ==(SpaceDate? left, SpaceDate? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(SpaceDate? left, SpaceDate? right)
+        {
+            return !Equals(left, right);
+        }
+
+        public int CompareTo(SpaceDate? other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            return string.Compare(Iso, other.Iso, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return 1;
+            if (ReferenceEquals(this, obj)) return 0;
+            return obj is SpaceDate other ? CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(SpaceDate)}");
         }
     }
 }
--- src/SpaceDotNet.Common/Types/SpaceTime.cs
+++ src/SpaceDotNet.Common/Types/SpaceTime.cs
@@ -6,7 +6,7 @@ using JetBrains.Annotations;
 namespace SpaceDotNet.Common.Types
 {
     [PublicAPI]
-    public class SpaceTime
+    public class SpaceTime : IComparable<SpaceTime>, IComparable
     {
         [JsonPropertyName("iso")]
         public string Iso { get; set; } = default!;
@@ -17,5 +17,47 @@ namespace SpaceDotNet.Common.Types
         public DateTime AsDateTime() => Timestamp.AsDateTime();
 
         public override string ToString() => AsDateTime().ToString("s", CultureInfo.InvariantCulture);
+
+        protected bool Equals(SpaceTime other)
+        {
+            return string.Equals(Iso, other.Iso, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((SpaceTime) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return StringComparer.OrdinalIgnoreCase.GetHashCode(Iso);
+        }
+
+        public static bool operator ==(SpaceTime? left, SpaceTime? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(SpaceTime? left, SpaceTime? right)
+        {
+            return !Equals(left, right);
+        }
+
+        public int CompareTo(SpaceTime? other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            return string.Compare(Iso, other.Iso, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return 1;
+            if (ReferenceEquals(this, obj)) return 0;
+            return obj is SpaceTime other ? CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(SpaceTime)}");
+        }
     }
 }
