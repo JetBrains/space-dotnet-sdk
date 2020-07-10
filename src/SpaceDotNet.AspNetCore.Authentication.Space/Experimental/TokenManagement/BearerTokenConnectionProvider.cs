4b0aafefb3554c4d46cc733cacc6bf06151eb9c3
Maarten Balliauw <maarten@maartenballiauw.be>
2020-07-10 13:10:27 +0200

    Remove redundant using statement

--- src/SpaceDotNet.AspNetCore.Authentication.Space/Experimental/TokenManagement/BearerTokenConnectionProvider.cs
+++ src/SpaceDotNet.AspNetCore.Authentication.Space/Experimental/TokenManagement/BearerTokenConnectionProvider.cs
@@ -2,7 +2,6 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SpaceDotNet.Common;
