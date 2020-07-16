2c4025adc2ab943eecd5ead2ae1b4026ac5516a7
Maarten Balliauw <maarten@maartenballiauw.be>
2020-07-16 09:08:20 +0200

    Make CSharpExpression.NullLiteral readonly

--- src/SpaceDotNet.Generator/CodeGeneration/CSharp/CSharpExpression.cs
+++ src/SpaceDotNet.Generator/CodeGeneration/CSharp/CSharpExpression.cs
@@ -2,6 +2,6 @@ namespace SpaceDotNet.Generator.CodeGeneration.CSharp
 {
     public static class CSharpExpression
     {
-        public static string NullLiteral = "null";
+        public static readonly string NullLiteral = "null";
     }
 }
