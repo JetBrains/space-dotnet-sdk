692d83a80458347e7197d1aa8c89fac99a2d09cd
Maarten Balliauw <maarten@maartenballiauw.be>
2020-11-05 21:33:37 +0100

    Reduce automation resource limits again

--- .space.kts
+++ .space.kts
@@ -1,15 +1,15 @@
 job("Run NUKE build") {
     container("mcr.microsoft.com/dotnet/core/sdk:3.1-bionic") {
         resources {
-            cpu = 4096
-            memory = 4096
+            cpu = 2048
+            memory = 2048
         }
 
         env.set("JB_SPACE_NUGET_URL", Params("SPACEDOTNET_NUGET_URL"))
         env.set("JB_SPACE_CLIENT_TOKEN", Secrets("SPACEDOTNET_NUGET_APIKEY"))
 
-        env.set("JB_SPACE_PUBLIC_NUGET_URL", Params("SPACEDOTNET_PUBLIC_NUGET_URL"))
-        env.set("JB_SPACE_PUBLIC_CLIENT_TOKEN", Secrets("SPACEDOTNET_PUBLIC_NUGET_APIKEY"))
+        env.set("JB_SPACE_PUBLIC_NUGET_URL", Params("spacedotnet_public_nuget_url"))
+        env.set("JB_SPACE_PUBLIC_CLIENT_TOKEN", Secrets("spacedotnet_public_nuget_apikey"))
 
         shellScript {
             content = """
