e9e93faf7ddd240dfcd1cf519ccf620f06c59e00
Maarten Balliauw <maarten@maartenballiauw.be>
2021-06-02 10:52:19 +0200

    Infrastructure: build in .NET 6.0 container

--- .space.kts
+++ .space.kts
@@ -5,6 +5,7 @@ val dotNetInstallScript = """
   
   .nuke/temp/dotnet-install.sh --channel 3.1 --install-dir /usr/share/dotnet
   .nuke/temp/dotnet-install.sh --channel 5.0 --install-dir /usr/share/dotnet
+  .nuke/temp/dotnet-install.sh --channel 6.0 --install-dir /usr/share/dotnet
 
 """.trimIndent()
 
@@ -13,7 +14,7 @@ job("Continuous integration build") {
         gitPush { enabled = true }
     }
     
-    container("mcr.microsoft.com/dotnet/sdk:5.0") {
+    container("mcr.microsoft.com/dotnet/sdk:6.0") {
         resources {
             cpu = 2.cpu
             memory = 2.gb
@@ -35,7 +36,7 @@ job("Build and publish to NuGet.org (manual)") {
         gitPush { enabled = false } // disable the default gitPush trigger
     }
     
-    container("mcr.microsoft.com/dotnet/sdk:5.0") {
+    container("mcr.microsoft.com/dotnet/sdk:6.0") {
         resources {
             cpu = 2.cpu
             memory = 2.gb
