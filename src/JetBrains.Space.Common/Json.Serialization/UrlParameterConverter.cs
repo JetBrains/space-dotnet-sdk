39a7be3102f5e6b66948b82b1bcd4e3138520939
Maarten Balliauw <maarten@maartenballiauw.be>
2021-11-03 17:11:03 +0100

    System.InvalidCastException: Unable to cast object of type UrlParameterConverter to type 'System.Text.Json.Serialization.JsonConverter`1[JetBrains.Space.Client.SprintIdentifier]' #4

--- src/JetBrains.Space.Common/Json.Serialization/UrlParameterConverter.cs
+++ src/JetBrains.Space.Common/Json.Serialization/UrlParameterConverter.cs
@@ -1,29 +1,51 @@
 using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using JetBrains.Annotations;
 using JetBrains.Space.Common.Types;
 
 namespace JetBrains.Space.Common.Json.Serialization;
 
 /// <summary>
+/// A <see cref="JsonConverterFactory"/> that can (de)serialize an instance of <see cref="IUrlParameter"/>.
+/// </summary>
+public class UrlParameterConverter : JsonConverterFactory
+{
+    private readonly Type _genericConverterType = typeof(UrlParameterConverter<>);
+    
+    /// <inheritdoc />
+    public override bool CanConvert(Type objectType)
+        => typeof(IUrlParameter).IsAssignableFrom(objectType);
+    
+    /// <inheritdoc />
+    public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    {
+        var constructedType = _genericConverterType.MakeGenericType(typeToConvert);
+        return Activator.CreateInstance(constructedType) as JsonConverter;
+    }
+}
+
+/// <summary>
 /// A <see cref="JsonConverter{T}"/> that can (de)serialize an instance of <see cref="IUrlParameter"/>.
 /// </summary>
-public class UrlParameterConverter : JsonConverter<object>
+/// <typeparam name="T">The <see cref="Type"/> to convert.</typeparam>
+public class UrlParameterConverter<T> : JsonConverter<T>
+    where T : class, IUrlParameter
 {
-    private static readonly Type UrlParameterType = typeof(IUrlParameter);
+    /// <inheritdoc />
+    public override bool CanConvert(Type objectType)
+        => typeof(IUrlParameter).IsAssignableFrom(objectType);
         
     /// <inheritdoc />
-    public override bool CanConvert(Type objectType) => objectType != UrlParameterType && UrlParameterType.IsAssignableFrom(objectType);
-        
-    /// <inheritdoc />
-    public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    [CanBeNull]
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        using var document = JsonDocument.ParseValue(ref reader);
-        return document.RootElement.Clone();
+        JsonDocument.ParseValue(ref reader).Dispose();
+        return null;
     }
 
     /// <inheritdoc />
-    public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
     {
         writer.WriteStringValue(value.ToString());
     }
---
+++ tests/JetBrains.Space.Client.Tests/Json.Serialization/UrlParameterConverterTests.cs
@@ -0,0 +1,72 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using JetBrains.Space.Common.Json.Serialization;
+using JetBrains.Space.Common.Types;
+using Xunit;
+
+namespace JetBrains.Space.Client.Tests.Json.Serialization;
+
+public class UrlParameterConverterTests
+{
+    [Theory]
+    [InlineData(typeof(SomeUrlParameter), true)]
+    [InlineData(typeof(object), false)]
+    public void CanConvertTests(Type requestedType, bool expectedResult)
+    {
+        // Arrange
+        var target = new UrlParameterConverter();
+
+        // Act
+        var result = target.CanConvert(requestedType);
+                
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+        
+    [Fact]
+    public void ReadReturnsNull()
+    {
+        // Arrange
+        var target = new UrlParameterConverter().CreateConverter(typeof(SomeUrlParameter), new JsonSerializerOptions()) as JsonConverter<SomeUrlParameter>;
+            
+        // Act & Assert
+        IUrlParameter? result = null;
+            
+        var utf8JsonBytes = Encoding.UTF8.GetBytes("{ }");
+        var reader = new Utf8JsonReader(utf8JsonBytes, true, new JsonReaderState());
+        while (reader.Read())
+        {
+            result = target!.Read(ref reader, typeof(SomeUrlParameter), new JsonSerializerOptions());
+        }
+            
+        Assert.Null(result);
+    }
+        
+    [Fact]
+    public void WriteTests()
+    {
+        // Arrange
+        var target = new UrlParameterConverter().CreateConverter(typeof(SomeUrlParameter), new JsonSerializerOptions()) as JsonConverter<SomeUrlParameter>;
+
+            
+        // Act & Assert
+        using var memoryStream = new MemoryStream();
+        var writer = new Utf8JsonWriter(memoryStream);
+        target!.Write(writer, new SomeUrlParameter(), new JsonSerializerOptions());
+        writer.Flush();
+        memoryStream.Position = 0;
+            
+        using var reader = new StreamReader(memoryStream);
+        var result = reader.ReadToEnd();
+            
+        Assert.Equal($"\"{nameof(SomeUrlParameter)}\"", result);
+    }
+
+    public sealed class SomeUrlParameter : IUrlParameter
+    {
+        public override string ToString() => nameof(SomeUrlParameter);
+    }
+}
