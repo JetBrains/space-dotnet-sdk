8a3cdeaa5bbfa2174767ee12f65a81cac993eb96
Maarten Balliauw <maarten@maartenballiauw.be>
2022-07-11 10:51:48 +0200

    Extract class

--- src/JetBrains.Space.Generator/CodeGeneration/CSharp/Extensions/ApiDocumentationExtensions.cs
+++ src/JetBrains.Space.Generator/CodeGeneration/CSharp/Extensions/ApiDocumentationExtensions.cs
@@ -1,27 +1,8 @@
-using System;
 using System.IO;
 using System.Text;
 using JetBrains.Space.Generator.Model.HttpApi;
 
 namespace JetBrains.Space.Generator.CodeGeneration.CSharp.Extensions;
-
-public static class ApiDocumentationUtilities
-{
-    public static void RenderCSharpDocumentation(
-        ApiDescription? description,
-        ApiExperimental? experimental,
-        Action<string> renderOutput)
-    {
-        if (description != null)
-        {
-            renderOutput(description.ToCSharpDocumentationSummary());
-        }
-        if (experimental != null)
-        {
-            renderOutput(experimental.ToCSharpDocumentation());
-        }
-    }
-}
 
 public static class ApiDocumentationExtensions
 {
