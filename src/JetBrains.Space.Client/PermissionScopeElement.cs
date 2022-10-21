397c508e14fa93f98f8782c727b2602c4eb7a929
Maarten Balliauw <maarten@maartenballiauw.be>
2022-10-21 08:10:38 +0200

    Add PermissionScope support

--- src/JetBrains.Space.Client/PermissionScopeElement.cs
+++ src/JetBrains.Space.Client/PermissionScopeElement.cs
@@ -9,12 +9,42 @@ namespace JetBrains.Space.Client;
 public class PermissionScopeElement
 {
     /// <summary>
-    /// THe context in which the permission applies.
+    /// The context in which the permission applies.
     /// </summary>
-    public PermissionContextIdentifier Context { get; set; } 
-    
+#if NET6_0_OR_GREATER
+    public PermissionContextIdentifier Context { get; init; }
+#else
+    public PermissionContextIdentifier Context { get; set; }
+#endif
+
     /// <summary>
     /// The permission identifier.
     /// </summary>
+#if NET6_0_OR_GREATER
+    public PermissionIdentifier Permission { get; init; }
+#else
     public PermissionIdentifier Permission { get; set; }
+#endif
+
+    /// <summary>
+    /// Creates a new <see cref="PermissionScopeElement"/>.
+    /// </summary>
+#pragma warning disable CS8618
+    public PermissionScopeElement()
+#pragma warning restore CS8618
+    {
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="PermissionScopeElement"/>.
+    /// </summary>
+    /// <param name="context">The context in which the permission applies.</param>
+    /// <param name="permission">The permission identifier.</param>
+    public PermissionScopeElement(
+        PermissionContextIdentifier context,
+        PermissionIdentifier permission)
+    {
+        Context = context;
+        Permission = permission;
+    }
 }
