a51bb8a348b40b2c92288efadbf479d4da4d6dfd
Maarten Balliauw <maarten@maartenballiauw.be>
2022-11-15 14:26:09 +0100

    Allow using target-typed new in PermissionScopeBuilder

--- src/JetBrains.Space.Client/PermissionScopeBuilder.cs
+++ src/JetBrains.Space.Client/PermissionScopeBuilder.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using JetBrains.Annotations;
 using JetBrains.Space.Common;
@@ -13,10 +14,20 @@ public static class PermissionScopeBuilder
     /// <summary>
     /// Build a <see cref="PermissionScope"/> based on a set of <see cref="PermissionScopeElement"/>.
     /// </summary>
-    /// <param name="elements">The <see cref="PermissionScopeElement"/> to build a scope for.</param>
+    /// <param name="element">The <see cref="PermissionScopeElement"/> to build a scope for.</param>
     /// <returns>A <see cref="PermissionScope"/> that represents the set of <see cref="PermissionScopeElement"/>.</returns>
-    public static PermissionScope Build(params PermissionScopeElement[] elements) =>
-        new(string.Join(" ", elements.Select(
-            // ReSharper disable once RedundantToStringCall
+    [SuppressMessage("ReSharper", "RedundantToStringCall")]
+    public static PermissionScope Build(PermissionScopeElement element) =>
+        new($"{element.Context}:{element.Permission}");
+
+    /// <summary>
+    /// Build a <see cref="PermissionScope"/> based on a set of <see cref="PermissionScopeElement"/>.
+    /// </summary>
+    /// <param name="element">The first <see cref="PermissionScopeElement"/> to build a scope for.</param>
+    /// <param name="elements">The other <see cref="PermissionScopeElement"/> to build a scope for.</param>
+    /// <returns>A <see cref="PermissionScope"/> that represents the set of <see cref="PermissionScopeElement"/>.</returns>
+    [SuppressMessage("ReSharper", "RedundantToStringCall")]
+    public static PermissionScope Build(PermissionScopeElement element, params PermissionScopeElement[] elements) =>
+        new(Build(element) + " " + string.Join(" ", elements.Select(
             it => it.Context.ToString() + ":" + it.Permission.ToString())));
 }
